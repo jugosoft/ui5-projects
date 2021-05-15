05adc24a4ba2a2df8c4c48fc1bc3b0b5b769b542
jugosoft <sardimka@gmail.com>
2021-05-15 12:13:29 +0400

    Step3 Control. Added Text to the index page

--- 4/webapp/index.js
+++ 4/webapp/index.js
@@ -1,6 +1,10 @@
 sap.ui.define([
-
-], function(){
+    "sap/m/Text"
+], function(Text){
     'use strict';
+    
     console.log('UI5 Framework ist bereit!');
+
+    var oText = new Text({text: 'UI5 Framework ist bereit!'});
+    oText.placeAt('content');
 })
