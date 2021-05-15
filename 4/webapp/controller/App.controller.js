4332b2a32b38a5f464dc95af29a8689d4400a177
jugosoft <sardimka@gmail.com>
2021-05-15 12:53:27 +0400

    Step7 Models. Added JSON Model

--- 4/webapp/controller/App.controller.js
+++ 4/webapp/controller/App.controller.js
@@ -1,10 +1,31 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
+    'sap/ui/model/json/JSONModel',
     'sap/m/MessageToast'
-], function(Controller, MessageToast) {
+], function(Controller, JSONModel, MessageToast) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.App", {
+        //Actions on Initialization of View
+        onInit: function() {
+            //simulates fetching JSON data
+            var oData = {
+                user: {
+                    name: 'Ivan',
+                    lastName: 'Ivanov',
+                    age: 24
+                }
+            };
+            
+            var oModel = new JSONModel(oData);
+            var oView = this.getView();
+
+            oView.setModel(oModel);
+        },
+
         onSayHi: function() {
+
+            
+
             MessageToast.show('Hallo, Kamerad!');
         },
         onSayBue: function () {
