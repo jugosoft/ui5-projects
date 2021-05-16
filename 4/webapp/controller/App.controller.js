130c13a3fed677d6a052334690ad4b030b552a6f
jugosoft <sardimka@gmail.com>
2021-05-16 10:27:04 +0400

    Step8 i18n configuration

--- 4/webapp/controller/App.controller.js
+++ 4/webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    'sap/m/MessageToast'
-], function(Controller, JSONModel, MessageToast) {
+    'sap/m/MessageToast',
+    'sap/ui/model/resource/ResourceModel'
+], function(Controller, JSONModel, MessageToast, ResourceModel) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         //Actions on Initialization of View
@@ -19,17 +20,35 @@ sap.ui.define([
             var oModel = new JSONModel(oData);
             var oView = this.getView();
 
+            //setting i18n model
+            var i18nModel = new ResourceModel({
+                bundleName: 'sap.ui.demo.walkthrough.i18n.i18n',
+                supportedLocales: [''],
+                fallbackLocale: ''
+            });
+
+            oView.setModel(i18nModel, 'i18n');
+
             oView.setModel(oModel);
         },
 
         onSayHi: function() {
-
-            
-
-            MessageToast.show('Hallo, Kamerad!');
+            //receiving msg from i18n model
+            var oView = this.getView();
+            var oBundle = oView.getModel('i18n').getResourceBundle();
+            var sName = oView.getModel().getProperty('/user/name');
+            var sMsg = oBundle.getText('hiMsg', [sName])
+            MessageToast.show(sMsg);
         },
+
+
         onSayBue: function () {
-            MessageToast.show('Aus WiederSehen!');
+            //receiving msg from i18n model
+            var oView = this.getView();
+            var oBundle = oView.getModel('i18n').getResourceBundle();
+            var sName = oView.getModel().getProperty('/user/name');
+            var sMsg = oBundle.getText('bueMsg', [sName])
+            MessageToast.show(sMsg);
         }
     });    
 });
