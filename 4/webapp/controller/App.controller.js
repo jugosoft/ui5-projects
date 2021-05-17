ddf3e735f085be331a4f7e37173a2e2e1065aacc
jugosoft <sardimka@gmail.com>
2021-05-17 10:11:39 +0400

    Step15 Nested Views

--- 4/webapp/controller/App.controller.js
+++ 4/webapp/controller/App.controller.js
@@ -1,28 +1,8 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast',    
-    'sap/ui/model/resource/ResourceModel'
-], function(Controller, MessageToast, ResourceModel) {
+    'sap/ui/core/mvc/Controller'
+], function(Controller) {
     'use strict';
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-        //Actions on Initialization of View were moved to the component :-)
-        onSayHi: function() {
-            //receiving msg from i18n model
-            var oView = this.getView();
-            var oBundle = oView.getModel('i18n').getResourceBundle();
-            var sName = oView.getModel().getProperty('/user/name');
-            var sMsg = oBundle.getText('hiMsg', [sName])
-            MessageToast.show(sMsg);
-        },
-
-
-        onSayBue: function () {
-            //receiving msg from i18n model
-            var oView = this.getView();
-            var oBundle = oView.getModel('i18n').getResourceBundle();
-            var sName = oView.getModel().getProperty('/user/name');
-            var sMsg = oBundle.getText('bueMsg', [sName])
-            MessageToast.show(sMsg);
-        }
+       
     });    
 });
---
+++ 4/webapp/controller/Intro.controller.js
@@ -0,0 +1,28 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller',
+    'sap/m/MessageToast'
+], function(Controller, MessageToast) {
+    'use strict';
+
+    return Controller.extend('sap.ui.demo.walkthrough.Intro', {
+        //Actions on Initialization of View were moved to the component :-)
+        onSayHi: function () {
+            //receiving msg from i18n model
+            var oView = this.getView();
+            var oBundle = oView.getModel('i18n').getResourceBundle();
+            var sName = oView.getModel().getProperty('/user/name');
+            var sMsg = oBundle.getText('hiMsg', [sName])
+            MessageToast.show(sMsg);
+        },
+
+        onSayBue: function () {
+            //receiving msg from i18n model
+            var oView = this.getView();
+            var oBundle = oView.getModel('i18n').getResourceBundle();
+            var sName = oView.getModel().getProperty('/user/name');
+            var sMsg = oBundle.getText('bueMsg', [sName])
+            MessageToast.show(sMsg);
+        }
+    });
+
+});
