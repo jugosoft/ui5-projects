e9a94ea2dbfc1d1af44d9d9fcfb9c16dd01bdcef
jugosoft <sardimka@gmail.com>
2021-06-06 13:46:32 +0400

    Refactoring

--- 4/webapp/controller/Intro.controller.js
+++ 4/webapp/controller/Intro.controller.js
@@ -4,25 +4,23 @@ sap.ui.define([
 ], function(Controller, MessageToast) {
     'use strict';
 
+    function saySomething (params) {
+        //receiving msg from i18n model
+        var oView = this.getView();
+        var oBundle = oView.getModel('i18n').getResourceBundle();
+        var sName = oView.getModel().getProperty('/user/name');
+        var sMsg = oBundle.getText(params.text, [sName])
+        MessageToast.show(sMsg);
+    };
+
     return Controller.extend('sap.ui.demo.walkthrough.Intro', {
         //Actions on Initialization of View were moved to the component :-)
         onSayHi: function () {
-            //receiving msg from i18n model
-            var oView = this.getView();
-            var oBundle = oView.getModel('i18n').getResourceBundle();
-            var sName = oView.getModel().getProperty('/user/name');
-            var sMsg = oBundle.getText('hiMsg', [sName])
-            MessageToast.show(sMsg);
+            saySomething.call(this, { text: 'hiMsg' });
         },
 
         onSayBue: function () {
-            //receiving msg from i18n model
-            var oView = this.getView();
-            var oBundle = oView.getModel('i18n').getResourceBundle();
-            var sName = oView.getModel().getProperty('/user/name');
-            var sMsg = oBundle.getText('bueMsg', [sName])
-            MessageToast.show(sMsg);
-        }
+            saySomething.call(this, { text: 'bueMsg' });
+        },
     });
-
 });
