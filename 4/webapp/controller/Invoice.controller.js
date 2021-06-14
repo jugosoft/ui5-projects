457c5e9b4143a65e3c70622ca40191443d6cb660
jugosoft <sardimka@gmail.com>
2021-06-14 16:06:24 +0400

    Step24. Filtering. Also removed useless components

--- 4/webapp/controller/Invoice.controller.js
+++ 4/webapp/controller/Invoice.controller.js
@@ -1,15 +1,31 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel',
-    'sap/ui/demo/walkthrough/model/formatter'
-], function(Controller, JSONModel, formatter) {
+    'sap/ui/demo/walkthrough/model/formatter',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator'
+], function(Controller, JSONModel, Formatter, Filter, FilterOperator) {
     'use strict';
     return Controller.extend('sap.ui.demo.walkthrough.Invoice', {
-        formatter: formatter,
+
+        formatter: Formatter,
+
         onInit: function () {
             var oModel = new JSONModel({ currency: 'USD' });
             var oView = this.getView();
             oView.setModel(oModel, 'view');
+        },
+
+        onFilterInvoices: function (oEvent) {
+            var aFilterOptions = [];
+            var sQuery = oEvent.getParameter('query');
+            
+            if (sQuery) 
+                aFilterOptions.push(new Filter('ProductName', FilterOperator.Contains, sQuery));
+            
+            var oList = this.byId('idInvoiceList');
+            var oBinding = oList.getBinding('items');
+            oBinding.filter(aFilterOptions);
         }
     });
 });
