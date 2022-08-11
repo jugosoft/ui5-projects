4fbecef04c56c1f0d61b9d6946a8211127e34738
DChernyh <sardimka@gmail.com>
2022-08-11 14:28:53 +0400

    i don't really care. i'm exhausted

--- demo-app/uimodule/webapp/controller/App.controller.js
+++ demo-app/uimodule/webapp/controller/App.controller.js
@@ -1,13 +1,22 @@
-sap.ui.define(
-    ["sap/ui/core/mvc/Controller"],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} BaseController
-     */
-    function (BaseController) {
-        "use strict";
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast",
+	"com/rus/development/demoApp/model/formatter"
+], function (Controller, MessageToast, formatter) {
+	"use strict";
 
-        return BaseController.extend("com.rus.development.demoApp.controller.controller.App", {
-            onInit() {},
-        });
-    }
-);
+	return Controller.extend("com.rus.development.demoApp.controller.App", {
+
+		formatter : formatter,
+
+		onShowHello : function () {
+			// read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
+		}
+	});
+});
